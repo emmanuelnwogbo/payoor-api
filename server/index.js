afa0f44213262423b85d772401dc9023d075e449
emmanuel <nerdyemmanuel@gmail.com>
2023-10-22 01:53:55 +0100

    make it better

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const corsOptions = {
     optionsSuccessStatus: 200,
 };
 
-//app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
